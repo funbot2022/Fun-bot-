4a6880b1315d3dd4e91ff75dd58f2dd23ae13869
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2022-06-02 14:37:22 -0500

    refactor emojis into variables

--- funbot/main.py
+++ funbot/main.py
@@ -7,6 +7,9 @@ from discord.ext import commands
 _EXTENSIONS = [
 ]
 
+_CHECKMK = "\u2705"
+_FAILMK = "\u274C"
+
 bot = commands.Bot(command_prefix="$")
 
 @bot.command(description="Reload bot", hidden=True)
@@ -14,13 +17,13 @@ async def reload(ctx):
     for ext in _EXTENSIONS:
         try:
             bot.reload_extension(ext)
-            # green checkmark
         except commands.errors.ExtensionNotLoaded:
-            # red X
-            await ctx.message.add_reaction("\u274C")
+            await ctx.message.add_reaction(_FAILMK)
             raise
 
-    await ctx.message.reply(f"\u2705 Reloaded {len(_EXTENSIONS)} extensions!")
+    await ctx.message.reply(
+        f"{_CHECKMK} Reloaded {len(_EXTENSIONS)} extensions!"
+    )
 
 def main():
     token = os.environ.get("DISCORD_BOT_TOKEN")
